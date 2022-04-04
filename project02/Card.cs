d4881b1273684666d7ccafbfd6b6732ae0010fd2
Baran Taylan <dev.barantaylan@gmail.com>
2022-04-05 00:23:12 +0300

    refactor with reviews

--- project02/Card.cs
+++ project02/Card.cs
@@ -4,6 +4,7 @@ namespace Project02;
 
 public class Card
 {
+    private int id;
     private string title;
     private string content;
     private int personId;
@@ -42,6 +43,23 @@ public class Card
         set => Line = value;
     }
 
+    public static Card AddCard(Card card)
+    {
+        int validId = Cards.Count > 0 ? Cards.Max(x => x.id) + 1 : 0; 
+        card.id = validId;
+
+        Cards.Add(card);
+
+        return card;
+    }
 
     public static List<Card> Cards = new List<Card>();
+
+    public void WriteCardInfo()
+    {
+        Console.WriteLine("Baslik      : " + Title + "\n" +
+            "Icerik      : " + Content + "\n" +
+            "Atanan Kisi : " + PersonId + "\n" +
+            "Buyukluk    : " + Size + "\n");
+    }
 }
