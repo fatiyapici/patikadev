6e512653878a66a918c6a567a549116fa2f60148
Fatih YAPICI <fa.yapici@gmail.com>
2022-05-23 15:57:10 +0300

    Weather Forecast WebApi Example
    Weather Forecast WebApi Example w/ ByID

---
+++ pratics/HelloWebapi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HelloWebapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]s")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        // [HttpGet]
+        // public ActionResult<WeatherForecast> Get()
+        // {
+        //     var rng = new Random();
+        //     return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //     {
+        //         Date = DateTime.Now.AddDays(index),
+        //         TemperatureC = rng.Next(-20, 55),
+        //         Summary = Summaries[rng.Next(Summaries.Length)]
+        //     })
+        //     .ToArray();
+        // }
+
+        [HttpGet("{id}")]
+        //Example usage : api/WeatherForecast?id=3
+        public ActionResult<WeatherForecast> GetById(string id)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray()[0];
+        }
+
+        // [HttpGet]
+        // //Example usage : api/WeatherForecast/3
+        // public IEnumerable<WeatherForecast> GetForecasts(string id)
+        // {
+        //     var rng = new Random();
+        //     return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //     {
+        //         Date = DateTime.Now.AddDays(index),
+        //         TemperatureC = rng.Next(-20, 55),
+        //         Summary = Summaries[rng.Next(Summaries.Length)]
+        //     })
+        //     .ToArray();
+        // }
+    }
+}
